fdf37cc5ea4b5e33aadf494a09f8396e25a6599d
Maria Fredriksson <mf223wk@student.lnu.se>
2023-04-17 17:26:32 +0200

    Added sceleton to ProjectTile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ProjectTile from './components/ProjectTile';
 
 function App() {
   return (
     <div className="App">
       <h1>Good Cause Finder</h1>
+      <ProjectTile />
     </div>
   );
 }
---
+++ src/components/ProjectTile.js
@@ -0,0 +1,12 @@
+
+const ProjectTile = () => {
+  return (
+    <div>
+      <h2>Project Title</h2>
+      <img src="./give-love.png" alt="Give love icon" />
+      <p>Project Description</p>
+    </div>
+  )
+}
+
+export default ProjectTile
