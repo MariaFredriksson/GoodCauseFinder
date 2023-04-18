cf123258c9beae796c10686bd28fba4407e253ae
Maria Fredriksson <mf223wk@student.lnu.se>
2023-04-18 17:45:51 +0200

    Fix the navbar sizing and position

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,49 +1,20 @@
 
-// const Navbar = () => {
-//   return (
-//     // <div>
-//     //   <h1>Good Cause Finder</h1>
-//     // </div>
-//     <nav className="navbar navbar-expand-lg sticky-top bg-body-tertiary">
-//       <div className="container-fluid">
-//         <a className="navbar-brand" href="#">Good Cause Finder</a>
-//         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-//           <span className="navbar-toggler-icon"></span>
-//         </button>
-//         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-//           <div className="navbar-nav">
-//             <a className="nav-link active" aria-current="page" href="#">Home</a>
-//             <a className="nav-link" href="#">Features</a>
-//             <a className="nav-link" href="#">Pricing</a>
-//           </div>
-//         </div>
-//       </div>
-//     </nav>
-//   )
-// }
-
-// export default Navbar
-
-
 const Navbar = () => {
   return (
+    // <div>
+    //   <h1>Good Cause Finder</h1>
+    // </div>
     <nav className="navbar navbar-expand-lg sticky-top bg-body-tertiary">
-      <div className="container-fluid">
-        <div className="row w-100">
-          <div className="col-6">
-            <a className="navbar-brand" href="#">Good Cause Finder</a>
-          </div>
-          <div className="col-6 text-end">
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div className="navbar-nav">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                <a className="nav-link" href="#">Features</a>
-                <a className="nav-link" href="#">Pricing</a>
-              </div>
-            </div>
+      <div className="container container-fluid">
+        <a className="navbar-brand" href="#">Good Cause Finder</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+            <a className="nav-link" href="#">Features</a>
+            <a className="nav-link" href="#">Pricing</a>
           </div>
         </div>
       </div>
@@ -51,4 +22,33 @@ const Navbar = () => {
   )
 }
 
-export default Navbar;
+export default Navbar
+
+
+// const Navbar = () => {
+//   return (
+//     <nav className="navbar navbar-expand-lg sticky-top bg-body-tertiary">
+//       <div className="container container-fluid">
+//         <div className="row w-100">
+//           <div className="col-6">
+//             <a className="navbar-brand" href="#">Good Cause Finder</a>
+//           </div>
+//           <div className="col-6 text-end">
+//             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+//               <span className="navbar-toggler-icon"></span>
+//             </button>
+//             <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+//               <div className="navbar-nav">
+//                 <a className="nav-link active" aria-current="page" href="#">Home</a>
+//                 <a className="nav-link" href="#">Features</a>
+//                 <a className="nav-link" href="#">Pricing</a>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </nav>
+//   )
+// }
+
+// export default Navbar;
