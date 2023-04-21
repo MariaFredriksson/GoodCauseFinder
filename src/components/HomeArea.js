40dcc144d876a9a5440344a248a5b814591b2560
Maria Fredriksson <mf223wk@student.lnu.se>
2023-04-21 13:14:13 +0200

    Add some content and style to project area and home area

--- src/components/HomeArea.js
+++ src/components/HomeArea.js
@@ -1,7 +1,12 @@
 
 const HomeArea = () => {
   return (
-    <div>HomeArea</div>
+    <div className="container">
+      <div className="home">
+        <h1>Welcome</h1>
+        <img src="./hand heart.png" className="home-image img-fluid" alt="Give love" />
+      </div>
+    </div>
   )
 }
 
--- src/components/ProjectArea.js
+++ src/components/ProjectArea.js
@@ -1,7 +1,15 @@
 
 const ProjectArea = () => {
   return (
-    <div>ProjectArea</div>
+    <div className="container text-center">
+      <h2>Project title</h2>
+      <img src="./give-love.png" alt="Give love" />
+      <p>Organization</p>
+      
+      <div>Project Description - Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim eligendi iusto minus ipsam laborum sapiente impedit soluta commodi eum veniam, explicabo sit eos animi excepturi assumenda accusantium dolores aspernatur doloremque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim eligendi iusto minus ipsam laborum sapiente impedit soluta commodi eum veniam, explicabo sit eos animi excepturi assumenda accusantium dolores aspernatur doloremque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim eligendi iusto minus ipsam laborum sapiente impedit soluta commodi eum veniam, explicabo sit eos animi excepturi assumenda accusantium dolores aspernatur doloremque!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim eligendi iusto minus ipsam laborum sapiente impedit soluta commodi eum veniam, explicabo sit eos animi excepturi assumenda accusantium dolores aspernatur doloremque!</div>
+
+      <button type="button" className="btn btn-info">Support this project</button>
+    </div>
   )
 }
 
