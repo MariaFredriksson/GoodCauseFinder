78b501642028bdb19c9e3832b60f98ab425fb118
Maria Fredriksson <mf223wk@student.lnu.se>
2023-04-24 08:21:25 +0200

    Add organization tile

--- src/components/OrganizationArea.js
+++ src/components/OrganizationArea.js
@@ -1,8 +1,12 @@
+import OrganizationTile from "./OrganizationTile"
 
 const OrganizationArea = () => {
   return (
     <div className="container">
       <h2>Organizations</h2>
+      <OrganizationTile />
+      <OrganizationTile />
+      <OrganizationTile />
       {/* // TODO: Make tiles/cards for each organization.  */}
     </div>
   )
---
+++ src/components/OrganizationTile.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom"
+
+const OrganizationTile = () => {
+  return (
+    <div className="organization-tile">
+      <div className="card mb-3 organization-card p-3">
+        <Link to={"/organization"} className="tile">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img src="./give-love.png" className="card-img-top" alt="Give love icon" />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title tile-header">Organization</h5>
+                <p className="card-text">Organization description - This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p className="card-text"><small className="text-body-secondary">Something else</small></p>
+              </div>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default OrganizationTile
