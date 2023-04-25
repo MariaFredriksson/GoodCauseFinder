8a2db68b2d8a3c632fff41c12eb4ec8926dcc7b5
Maria Fredriksson <mf223wk@student.lnu.se>
2023-04-25 17:30:44 +0200

    Add white text shadow to hero in home are

--- src/components/HomeArea.js
+++ src/components/HomeArea.js
@@ -9,7 +9,7 @@ const HomeArea = () => {
           {/* <img src="./hand heart.png" className="home-image img-fluid" alt="Give love" /> */}
         </div>
         <div className="hero">
-          <div className="px-4 py-5 my-5 text-center">
+          <div className="px-4 py-5 my-5 hero-text text-center">
             <img className="d-block mx-auto mb-4" src="./give-love.png" alt="" width="72" />
               <h1 className="display-5 fw-bold text-body-emphasis">Something important</h1>
               <div className="col-lg-6 mx-auto">
