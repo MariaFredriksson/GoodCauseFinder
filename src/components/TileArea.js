6396bf3d882b27f4b1ed6886551976d44ab8f875
Maria Fredriksson <mf223wk@student.lnu.se>
2023-05-08 08:23:05 +0200

    Fix tile height + not fix footer

--- src/components/TileArea.js
+++ src/components/TileArea.js
@@ -9,8 +9,8 @@ const TileArea = () => {
 
   return (
     <div className="container">
-      <h2>Tile Area</h2>
-      <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-4">
+      {/* <h2>Tile Area</h2> */}
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-4 my-4">
         {/* Conditional rendering, so these tiles only shows when the fetch is done and there are some values */}
         {/* Loop though the projects array and send the data to each tile */}
         {data && data.projects.map(project => (
