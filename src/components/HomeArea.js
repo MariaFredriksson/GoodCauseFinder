4472f5551ce7ea1f11783e40042f50f2a0012ec8
Maria Fredriksson <mf223wk@student.lnu.se>
2023-06-04 10:25:30 +0200

    Update HomeArea

--- src/components/HomeArea.js
+++ src/components/HomeArea.js
@@ -11,17 +11,17 @@ const HomeArea = () => {
     <div className="home">
       <div className="container">
         {/* // TODO: Fixa så att denna div går att ta bort utan att bakgrundsbilden inte går upp hela vägen till navbaren */}
-        <div>
+        <div className="stupid-div">
           <p>.</p>
         </div>
         <div className="hero">
           <div className="px-4 py-5 my-5 hero-text text-center">
             <img className="d-block mx-auto mb-4" src={GiveLoveIcon} alt="" width="72" />
-              <h1 className="display-5 fw-bold text-body-emphasis">Something important</h1>
+              <h1 className="display-5 fw-bold text-body-emphasis">Dina hjärtefrågor gör skillnad!</h1>
               <div className="col-lg-6 mx-auto">
-                <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+              <p className="lead mb-4">Tillsammans förändrar vi världen! Utforska Good Cause Finder och ge kraft åt dina hjärtefrågor. Hitta biståndsprojekt som betyder något för dig, klicka och ge en gåva. Var med och skapa en bättre framtid - varje handling räknas!</p>
                 <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <Button link={"/projects"} text={"Call to action"} />
+                <Button link={"/projects"} text={"Gör skillnad!"} />
                 </div>
               </div>
           </div>
